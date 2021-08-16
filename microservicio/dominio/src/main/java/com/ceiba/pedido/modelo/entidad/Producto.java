650d0031f282b1be14ff023ef21d373f1c89deb9
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-08-16 15:20:41 -0500

    Validar creacion del la entidad producto

--- microservicio/dominio/src/main/java/com/ceiba/pedido/modelo/entidad/Producto.java
+++ microservicio/dominio/src/main/java/com/ceiba/pedido/modelo/entidad/Producto.java
@@ -1,11 +1,29 @@
 package com.ceiba.pedido.modelo.entidad;
 
+import java.util.Arrays;
+
+import com.ceiba.dominio.ValidadorArgumento;
+
 import lombok.Getter;
 
 @Getter
 public class Producto {
-	
+
+	private static final String EL_NOMBRE_ES_OBLIGATORIO = "El nombre es obligatorio";
+	private static final String EL_PRECIO_ES_OBLIGATORIO = "El precio es obligatorio";
+
 	private String nombre;
 	private Double precio;
-	
+
+	public static Producto crear(String nombre, Double precio) {
+		ValidadorArgumento.validarNoVacio(Arrays.asList(nombre), EL_NOMBRE_ES_OBLIGATORIO);
+		ValidadorArgumento.validarPositivo(precio, EL_PRECIO_ES_OBLIGATORIO);
+		return new Producto(nombre, precio);
+	}
+
+	private Producto(String nombre, Double precio) {
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
 }
